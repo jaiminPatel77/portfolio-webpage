de764b912d234e3fa586dce62ab2fe53ff6cdecd
csjaiminpatel <jaimin.patel@confidosoft.in>
2024-11-16 18:29:51 +0530

    Squashed commit of the following:
    commit 31510fc74f113ffb80ab13831f57956d311b5180
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Sat Nov 16 18:24:58 2024 +0530
    
        certificate added
    
    commit 3cfe5c54fe023813d92b67c528d19307cbd5d634
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Sat Nov 16 17:55:12 2024 +0530
    
        project details changed
    
    commit c2c37e68fcdc6793bfd054ca5fd30112867ef24a
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Sat Nov 16 14:05:14 2024 +0530
    
        certification added
    
    commit dce170aa98a2ba080f28d63f987aff71ddb3fb53
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Sat Nov 16 10:22:34 2024 +0530
    
        class override
        -- solved min-width issue
    
    commit 2db77bc4da0b16a696de7bf0078a3b0f03c4a7fa
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Wed Nov 13 16:28:02 2024 +0530
    
        img related changes
    
    commit 777bf5ae71e2779ecfee248a29ed477886e4b953
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Wed Nov 13 15:04:33 2024 +0530
    
        project title & scroll fix , CV updated
    
    commit 323f198800a6d97d9f527ae1c89dcb5ab56e5c37
    Author: csjaiminpatel <jaimin.patel@confidosoft.in>
    Date:   Wed Nov 13 08:58:16 2024 +0530
    
        temp-data

--- src/app/components/home/projects/projects.component.ts
+++ src/app/components/home/projects/projects.component.ts
@@ -12,71 +12,16 @@ export class ProjectsComponent implements OnInit {
   Projects = [
     {
       imgs:[
-        '../../../../assets/images/medmatch/screenshot.png',
-        '../../../../assets/images/medmatch/screenshot (1).png',
-        '../../../../assets/images/medmatch/screenshot (2).png',
-        '../../../../assets/images/medmatch/screenshot (3).png'],
-      Title:'Med Match',
-      Description:'The purpose of this project is to enhance the safety of prescribing drugs and to address the challenges related to prescribing errors which can have significant damage the patient’s health, the project ultimate goal is to contribute to the overall improvement of medication safety in the healthcare department.',
-      Technologies:['Angular','SASS','Angular-Material' , 'RxJS', 'Node.js'],
+        '../../../../assets/images/projects/MERN-001.jpg',
+        '../../../../assets/images/projects/MERN-003.jpg',
+        '../../../../assets/images/projects/MERN-002.jpg',
+        '../../../../assets/images/projects/MERN-004.jpg'],
+      Title:'PeerHub',
+      Description:'PeerHub is a fullstack social networking platform designed to connect users in a dynamic, interactive environment. Built with the MERN stack, it features user authentication and secure data management. Users can create profiles, upload images, and interact through comments and likes, simulating a modern social media experience.',
+      Technologies:['MongoDB','Express.js' ,'RxJS', 'Node.js' , 'JWT'],
       // ghLink:'https://github.com/jaimineldin/medmatch',
       // demoLink:'https://medmatchh.netlify.app/'
     },
-
-    {
-      imgs:[
-        '../../../../assets/images/todo-app/1.PNG',
-        '../../../../assets/images/todo-app/2.PNG',
-        '../../../../assets/images/todo-app/3.PNG',
-      ],
-      Title:'Dynamic Todo App',
-      Description:'Users can add, edit, delete, and prioritize tasks. Each task typically contains a title, description, due date, and status. The app allows users to reorder tasks by dragging and dropping them into different categories or positions, making it easy to organize and prioritize tasks.',
-      Technologies:['Angular','SCSS','Angular-Material' , 'RxJS'],
-      // ghLink:'https://github.com/jaimineldin/dynamic-todo-app',
-      // demoLink:'https://dynamic-todo-app.netlify.app/'
-    },
-    {
-      imgs:[
-      
-        '../../../../assets/images/recipe-book/screenshot (1).png',
-        '../../../../assets/images/recipe-book/screenshot (2).png',
-        '../../../../assets/images/recipe-book/screenshot (3).png',
-      ],
-      Title:'Recipe Book',
-      Description:'Users can add new recipes to their collection and categorize them based on different types of cuisines or meal types. Each recipe includes a comprehensive list of ingredients required for the dish, making it convenient for users to shop for the necessary items.',
-      Technologies:['Angular','Css','Angular-Material' , 'RxJS','firebase'],
-      // ghLink:'https://github.com/jaimineldin/recipe-book',
-      // demoLink:'https://recipe-book-theta-blue.vercel.app'
-    },
-    {
-      imgs:[
-        '../../../../assets/images/design-show/1.png',
-        '../../../../assets/images/design-show/2.png',
-        '../../../../assets/images/design-show/4.PNG',
-      ],
-      Title:'Design Show',
-      Description:'Design Show provides a user-friendly interface that enables artists and designers to upload high-quality images and multimedia content, along with detailed project descriptions. This allows viewers to explore the creative process and context behind each artwork.',
-      Technologies:['Html','SCSS','JavaScript' , 'Jquery', 'animation-libraries'],
-      ghLink:'',
-      // demoLink:'https://design-show.netlify.app/'
-    },
-
-  
-    {
-      imgs:[
-      
-        '../../../../assets/images/typing-speed/1.png',
-   
-      ],
-      Title:'Typing Speed Test',
-      Description:` The application measures the number of words or characters typed per minute (words per minute - WPM or characters per minute - CPM) to determine the user's typing speed. In addition to speed, the test evaluates typing accuracy by calculating the number of errors made during the typing exercise.`,
-      Technologies:['Html','CSS','JavaScript' , 'Jquery'],
-      // ghLink:'https://github.com/jaimineldin/typing-speed-test',
-      // demoLink:'https://typingspeedgamee.netlify.app/'
-    },
-
-    
-
 
   ]
   customOptions: OwlOptions = {
